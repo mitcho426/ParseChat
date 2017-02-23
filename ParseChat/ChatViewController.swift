f5f90667484a8093716bd9300ca3958783eb1c4a
Edison L <elam3@mail.ccsf.edu>
2017-02-22 20:07:52 -0800

    in the middle of: Query Parse for all messages using the Message class.

--- ParseChat/ChatViewController.swift
+++ ParseChat/ChatViewController.swift
@@ -9,16 +9,36 @@
 import UIKit
 import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var chatMessage: UITextField!
     
+    var messages : [PFObject]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "onTimer", userInfo: nil, repeats: true)
+        
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages?.count ?? 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! messageCell
+        
+        let message = messages?[indexPath.row]
+        let text = message?.value(forKey: "text") as? String
+        
+        cell.messageLabel.text = text
+        
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -38,6 +58,20 @@ class ChatViewController: UIViewController {
         }
     }
     
+    func onTimer() {
+        let query = PFQuery(className:"Message")
+        query.findObjectsInBackground { (response: [PFObject]?, error: Error?) in
+            if error != nil {
+                print("error: \(error)")
+            } else {
+                //print("response\(response)")
+                self.messages = response!
+                self.tableView.reloadData()
+                
+                self.tableViewScrollToBottom(animated: true)
+            }
+        }
+    }
     
     /*
     // MARK: - Navigation
